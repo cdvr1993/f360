398d94e4f982440e918f15b83891d63b3fa6f595
Cristian David Velazquez Ramirez <cdvr1993@gmail.com>
2018-06-17 12:12:36 -0700

    Fix issues with models & add fixtures

--- backend/surveys/models/participant.py
+++ backend/surveys/models/participant.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from surveys.models.company import Company
 from surveys.models.generation import Generation
 from surveys.models.person import Person
 from surveys.models.survey import Survey
 
 
 class Participant(Person):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     generation = models.ForeignKey(Generation, on_delete=models.CASCADE)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, null=True)
--- backend/surveys/models/person.py
+++ backend/surveys/models/person.py
@@ -5,7 +5,7 @@ class Person(models.Model):
     class Meta:
         abstract = True
 
-    answered = models.BooleanField()
+    answered = models.BooleanField(default=False)
     name = models.CharField(max_length=80)
     email = models.CharField(max_length=256)
     job = models.CharField(max_length=80)
